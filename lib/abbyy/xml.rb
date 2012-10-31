f5b8044e306e9e186f108438af4297b495c7d4f4
Vincent Durand <vincent.durand@madwork.org>
2012-10-31 18:31:31 +0100

    Parse XML response with many tasks as Array of task or single one

--- lib/abbyy/xml.rb
+++ lib/abbyy/xml.rb
@@ -1,18 +1,13 @@
 module Abbyy
   module XML
     def parse_task(resource)
-      Hash.new.tap do |task|
-        xml_data = REXML::Document.new(resource)
-        task[:id] = xml_data.elements["response/task"].attributes["id"]
-        task[:status] = xml_data.elements["response/task"].attributes["status"]
-        task[:resultUrl] = xml_data.elements["response/task"].attributes["resultUrl"]
-        task[:estimatedProcessingTime] = xml_data.elements["response/task"].attributes["estimatedProcessingTime"]
-        task[:description] = xml_data.elements["response/task"].attributes["description"]
-        task[:error] = xml_data.elements["response/task"].attributes["error"]
-        task[:credits] = xml_data.elements["response/task"].attributes["credits"]
-        task[:filesCount] = xml_data.elements["response/task"].attributes["filesCount"]
-        task[:statusChangeTime] = xml_data.elements["response/task"].attributes["statusChangeTime"]
-        task[:registrationTime] = xml_data.elements["response/task"].attributes["registrationTime"]
+      xml_data = REXML::Document.new(resource)
+      if xml_data.elements["response"].size > 1
+        Array.new.tap do |tasks|
+          xml_data.elements["response"].each { |response| tasks << build_task(response) }
+        end
+      else
+        build_task(xml_data.elements["response/task"])
       end
     end
     
@@ -23,6 +18,23 @@ module Abbyy
         xml_data = REXML::Document.new(resource.http_body)
         error.code = resource.http_code
         error.message = xml_data.elements["error/message"].text
+      end
+    end
+    
+    private
+    
+    def build_task(xml_data)
+      Hash.new.tap do |task|
+        task[:id] = xml_data.attributes["id"]
+        task[:status] = xml_data.attributes["status"]
+        task[:resultUrl] = xml_data.attributes["resultUrl"]
+        task[:estimatedProcessingTime] = xml_data.attributes["estimatedProcessingTime"]
+        task[:description] = xml_data.attributes["description"]
+        task[:error] = xml_data.attributes["error"]
+        task[:credits] = xml_data.attributes["credits"]
+        task[:filesCount] = xml_data.attributes["filesCount"]
+        task[:statusChangeTime] = xml_data.attributes["statusChangeTime"]
+        task[:registrationTime] = xml_data.attributes["registrationTime"]
       end
     end
   end
