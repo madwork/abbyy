1b43d30e4fcf59357744f84291d740b2c832294f
Vincent Durand <vincent.durand@madwork.org>
2012-10-31 12:33:20 +0100

    Rescue RestClient exceptions with execute method and set status instance variable

--- lib/abbyy/api.rb
+++ lib/abbyy/api.rb
@@ -1,20 +1,48 @@
-require 'abbyy/xml'
-
 module Abbyy
   module API
-    include Abbyy::XML
     
-    def process_image(image_path, options = {})
-      task(RestClient.post("#{@url}/processImage", options.merge(:upload => { :file => File.new(image_path, 'r') })))
+    API_METHODS = %w(process_image process_business_card get_task_status submit_image process_document).map(&:to_sym)
+    
+    def execute(sym, *args, &block)
+      self.task_status = send("run_#{sym}", *args, &block)
+      @status
+    rescue RestClient::BlockedByWindowsParentalControls => ex
+      raise Abbyy::IncorrectParameters.new(error(ex).message)
+    rescue RestClient::RequestFailed => ex
+      raise Abbyy::ProcessingFailed.new(error(ex).message)
     end
     
-    def process_business_card(image_path, options = {})
-      task(RestClient.post("#{@url}/processBusinessCard", options.merge(:upload => { :file => File.new(image_path, 'r') })))
+    def method_missing(sym, *args, &block)
+      API_METHODS.include?(sym) ? execute(sym, *args, &block) : super
     end
     
-    def get_task_status(task_id)
-      task(RestClient.get("#{@url}/getTaskStatus?taskid=#{task_id}"))
+    private
+    
+    # http://ocrsdk.com/documentation/apireference/processImage/
+    def run_process_image(image_path, options = {})
+      RestClient.post("#{@url}/processImage", options.merge(:upload => { :file => File.new(image_path, 'r') }))
     end
+    
+    # http://ocrsdk.com/documentation/apireference/processBusinessCard/
+    def run_process_business_card(image_path, options = {})
+      RestClient.post("#{@url}/processBusinessCard", options.merge(:upload => { :file => File.new(image_path, 'r') }))
+    end
+    
+    # http://ocrsdk.com/documentation/apireference/submitImage/
+    def run_submit_image(image_path, options = {})
+      RestClient.post("#{@url}/submitImage", options.merge(:upload => { :file => File.new(image_path, 'r') }))
+    end
+    
+    # http://ocrsdk.com/documentation/apireference/getTaskStatus/
+    def run_get_task_status(task_id = @status[:id])
+      RestClient.get("#{@url}/getTaskStatus?taskId=#{task_id}")
+    end
+    
+    # http://ocrsdk.com/documentation/apireference/processDocument/
+    def run_process_document(task_id = @status[:id])
+      RestClient.get("#{@url}/processDocument?taskId=#{task_id}")
+    end
+    
   end
 end
 
